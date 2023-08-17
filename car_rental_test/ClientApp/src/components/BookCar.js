25713e9260481a24ac2b11df821d19cbaf5d59a1
07Onkar <sd19_shiradhonkar_onkar@mgmcen.ac.in>
2023-08-17 15:00:09 +0530

    Added BookCar And done Minor Changes in PlanTrip Component

--- car_rental_test/ClientApp/src/components/BookCar.js
+++ car_rental_test/ClientApp/src/components/BookCar.js
@@ -148,7 +148,318 @@ class BookCar extends Component {
 
     return (
       <>
-        {/* Your JSX content here */}
+        <section id="booking-section" className="book-section">
+        {/* Overlay */}
+        <div
+          onClick={this.openModal}
+          className={`modal-overlay ${modal ? "active-modal" : ""}`}
+        ></div>
+
+        <div className="container">
+          <div className="book-content">
+            <div className="book-content_box">
+              <h2>Book a Car</h2>
+
+              <p className="error-message">
+                All Fields Required! <i className="fa-solid fa-xmark"></i>
+              </p>
+
+              <p className="booking-done">
+                Check Your Email to confirm an Order.{" "}
+                <i onClick={this.hideMessage} className="fa-solid fa-cmark"></i>
+              </p>
+
+              <form className="box-form">
+                <div className="box-form_car-type">
+                  <label>
+                    <i className="fa-solid fa-car"></i> &nbsp; Select Your Car
+                    Type
+                    <b>*</b>
+                  </label>
+                  <select value={carType} onChange={this.handleCar}>
+                    <option>Select Your Car Type</option>
+                    <option value="Audi Q7"> Audi A7 </option>
+                    <option value="Chevrolet Camaro"> Chevrolet Camaro </option>
+                    <option value="McLeren P7"> McLeren P7 </option>
+                    <option value="Ferrari LaFerrari">
+                      {" "}
+                      Ferrari LaFerrari{" "}
+                    </option>
+                    <option value="Lamborghini Aventador">
+                      {" "}
+                      Lamborghini Aventador{" "}
+                    </option>
+                    <option value="Porsche Panamera turbo">
+                      {" "}
+                      Porsche Panamera turbo{" "}
+                    </option>
+                  </select>
+                </div>
+                <div className="box-form_car-type">
+                  <label>
+                    <i className="fa-solid fa-location-dot"></i> &nbsp; Pick-up{" "}
+                    <b>*</b>
+                  </label>
+                  <select value={pickUp} onChange={this.handlePick}>
+                    <option>Select Your Pick Up Location</option>
+                    <option>Mumbai</option>
+                    <option>Pune</option>
+                    <option>Delhi</option>
+                    <option>Jaipur</option>
+                    <option>Chennai</option>
+                  </select>
+                </div>
+
+                <div className="box-form_car-type">
+                  <label>
+                    <i className="fa-solid fa-location-dot"></i> &nbsp; Drop-of{" "}
+                    <b>*</b>
+                  </label>
+                  <select value={dropOff} onChange={this.handleDrop}>
+                    <option>Select drop off location</option>
+                    <option>Mumbai</option>
+                    <option>Pune</option>
+                    <option>Delhi</option>
+                    <option>Jaipur</option>
+                    <option>Chennai</option>
+                  </select>
+                </div>
+
+                <div className="box-form_car-time">
+                  <label htmlFor="picktime">
+                    <i className="fa-regular fa-calendar-days "></i> &nbsp;
+                    Pick-up <b>*</b>
+                  </label>
+                  <input
+                    type="date"
+                    id="picktime"
+                    value={pickTime}
+                    onChange={this.handlePickTime}
+                  />
+                </div>
+
+                <div className="box-form_car-time">
+                  <label htmlFor="droptime">
+                    <i className="fa-regular fa-calendar-days "></i> &nbsp;
+                    Drop-of <b>*</b>
+                  </label>
+                  <input
+                    type="date"
+                    id="droptime"
+                    value={dropTime}
+                    onChange={this.handleDropTime}
+                  />
+                </div>
+
+                <button onClick={this.openModal} type="submit">
+                  Search
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+
+      {/* Modal */}
+      <div className={`booking-modal ${modal ? "acive-modal" : ""}`}>
+        {/* title */}
+        <div className="booking-modal_title">
+          <h2>Complete Reservation</h2>
+          <i onClick={this.openModal} className="fa-solid fa-xmark"></i>
+        </div>
+
+        {/* Message */}
+        <div className="booking-modal_message">
+          <h4>
+            <i className="fa-solid fa-circle-info"></i>
+            Upon Completing this reservation enquiry , You will recieve:
+          </h4>
+          <p>
+            Your Rental Voucher to produce on arrival at the rental desk and a
+            toll-free customer support Number.
+          </p>
+        </div>
+
+        {/* Car Info */}
+        <div className="booking-modal_car-info">
+          <div className="dates-div">
+            <div className="booking-modal_car-info_dates">
+              <h5>Location & Date</h5>
+              <span>
+                <i className="fa-solid fa-location-dot"></i>
+                <div>
+                  <h6>Pick-Up Date & Time</h6>
+                  <p>
+                    {pickTime} / <input type="time" className="input-time" />
+                  </p>
+                </div>
+              </span>
+            </div>
+
+            <div className="booking-modal_car-info_dates">
+              <span>
+                <i className="fa-solid fa-location-dot"></i>
+                <div>
+                  <h6>Drop-Off Date & Time</h6>
+                  <p>
+                    {dropTime} / <input type="time" className="input-time" />
+                  </p>
+                </div>
+              </span>
+            </div>
+
+            <div className="booking-modal_car-info_dates">
+              <span>
+                <i className="fa-solid fa-calendar-days"></i>
+                <div>
+                  <h6>Pick-Up Location</h6>
+                  <p>{pickUp}</p>
+                </div>
+              </span>
+            </div>
+
+            <div className="booking-modal_car-info_dates">
+              <span>
+                <i className="fa-solid fa-calendar-days"></i>
+                <div>
+                  <h6>Drop-Off Location</h6>
+                  <p>{dropOff}</p>
+                </div>
+              </span>
+            </div>
+          </div>
+
+          <div className="booking-modal_car-info_model">
+            <h5>
+              <span>Car -</span> {carType}
+            </h5>
+            {imgUrl && <img src={imgUrl} alt="car_img" />}
+          </div>
+        </div>
+
+        {/* personal Info */}
+
+        <div className="booking-modal_person-info">
+          <h4>Personal Information</h4>
+          <form className="info-form">
+            <div className="info-form_2col">
+              <span>
+                <label>
+                  First Name <b>*</b>
+                </label>
+                <input
+                  type="text"
+                  value={name}
+                  onChange={this.handleName}
+                  placeholder="Enter Your First Name"
+                />
+                <p className="error-modal">This is Required Field.</p>
+              </span>
+
+              <span>
+                <label>
+                  Last Name <b>*</b>
+                </label>
+                <input
+                  type="text"
+                  value={lastName}
+                  onChange={this.handleLastName}
+                  placeholder="Enter Your Last Name"
+                />
+                <p className="error-modal">This is Required Field.</p>
+              </span>
+
+              <span>
+                <label>
+                  Phone Number <b>*</b>
+                </label>
+                <input
+                  type="tel"
+                  value={phone}
+                  onChange={this.handlePhone}
+                  placeholder="Enter Your Phone Number"
+                />
+                <p className="error-modal">This is Required Field.</p>
+              </span>
+
+              <span>
+                <label>
+                  Age <b>*</b>
+                </label>
+                <input
+                  type="number"
+                  value={age}
+                  onChange={this.handleAge}
+                  placeholder="18"
+                />
+                <p className="error-modal">This is Required Field.</p>
+              </span>
+            </div>
+
+            <div className="info-form_1col">
+              <span>
+                <label>
+                  Email <b>*</b>
+                </label>
+                <input
+                value={email}
+                  type="email"
+                  onChange={this.handleEmail}
+                  placeholder="Enter your Email Address"
+                />
+                <p className="error-modal">This is Required Field.</p>
+              </span>
+
+              <span>
+                <label>
+                  Address <b>*</b>
+                </label>
+                <input
+                  value={address}
+                  type="text"
+                  onChange={this.handleAddress}
+                  placeholder="Enter your Street Address"
+                />
+                <p className="error-modal">This is Required Field.</p>
+              </span>
+
+              <span>
+                <label>
+                  City <b>*</b>
+                </label>
+                <input
+                value={city}
+                  type="text"
+                  onChange={this.handleCity}
+                  placeholder="Enter your City"
+                />
+                <p className="error-modal">This is Required Field.</p>
+              </span>
+
+              <span>
+                <label>
+                  Zip Code <b>*</b>
+                </label>
+                <input
+                  type="text"
+                  onChange={this.handleZip}
+                  value={zip}
+                  placeholder="Enter your Zip Code"
+                />
+                <p className="error-modal">This is Required Field.</p>
+              </span>
+            </div>
+            <span className="info-form_checkbox">
+              <input type="checkbox" />
+              <p>Please Send me latest <strong> News & Updates </strong></p>
+            </span>
+
+            <div className="reverse-button">
+              <button onClick={this.confirmBooking}>Reserve Now</button>
+            </div>
+          </form>
+        </div>
+      </div>
       </>
     );
   }
--- car_rental_test/ClientApp/src/components/pages/Layout.js
+++ car_rental_test/ClientApp/src/components/pages/Layout.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react';
-import { Container } from 'reactstrap';
-import { NavMenu } from './NavMenu';
+import { Home } from '../Home';
+import BookCar from '../BookCar';
+import ScrollTop from '../ScrollTop';
+import { NavMenu } from '../NavMenu';
+import { PlanTrip } from '../PlanTrip';
+
 
 export class Layout extends Component {
   static displayName = Layout.name;
@@ -8,10 +12,11 @@ export class Layout extends Component {
   render() {
     return (
       <div>
-        <NavMenu />
-        <Container tag="main">
-          {this.props.children}
-        </Container>
+        <NavMenu/>
+        <Home />
+            <BookCar />
+        <PlanTrip/>
+        <ScrollTop/>
       </div>
     );
   }
